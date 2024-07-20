0cae536c91abb925f2e1b31644461d90eb802a2f
Marcos Suarez <marcos.suarez1@estudiante.ceibal.edu.uy>
2024-07-20 18:06:01 -0300

    Se resolvio otro conflicto.

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,9 @@
 <?php
 
+use App\Http\Controllers\PersonaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< Updated upstream
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-=======
 Route::post('/usuario', [PersonaController::class, 'Crear']);
 Route::delete('/usuario/{d}', [PersonaController::class, 'Eliminar']);
 Route::put('/usuario/{d}', [PersonaController::class, 'Modificar']);
->>>>>>> Stashed changes
