1698aa627e1473941f6df36d66c45f6a1d43b0b0
Marcos Suarez <marcos.suarez1@estudiante.ceibal.edu.uy>
2024-07-20 16:55:48 -0300

    se creo la funcion crear usuario en el controlador y se arego la ruta en la API

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PersonaModel;
 use Illuminate\Http\Request;
 
 class PersonaController extends Controller
 {
-    //
+
+    protected $table = "usuario";
+
+    public function Crear(Request $request)
+        {
+            if ($request->has("nombre") && $request->has("apellido")) {
+    
+                $usuario = new PersonaModel();
+                $usuario->nombre = $request->post("nombre");
+                $usuario->apellido = $request->post("apellido");
+                $usuario->telefono = $request->post("telefono");
+                $usuario->save();
+                return $usuario;
+            }
+return response()->json(["error mesage" => "Error"]);
+    }
+
 }
